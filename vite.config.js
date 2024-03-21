595241f0e762bc4d00c35d14fe916a13612c3d4f
hashtagasp <151463864+hashtagasp@users.noreply.github.com>
2024-03-21 18:17:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   ],
-  plugins: [vue(),],
+  plugins: [vue()],
   base: '/hashtagasp.github.io',
   resolve: {
     alias: {
